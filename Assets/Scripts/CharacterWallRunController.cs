1f9a4b593c4339afd99f3a57589e639fa36e2305
Zoinks <murarashuvlad@gmail.com>
2023-10-31 00:28:43 +0200

    updated wall run controller

--- Assets/Scripts/CharacterWallRunController.cs
+++ Assets/Scripts/CharacterWallRunController.cs
@@ -1,55 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class CharacterWallRunController : MonoBehaviour
 {
-    [Tooltip("The distance at which the player character checks for walls to wall run on.")]
-    [Range(0.1f, 2f)]
-    [SerializeField]
-    private float wallCheckDistance = 0.75f; // m
+    [field: Range(0.1f, 2f)]
+    [field: SerializeField]
+    public float WallCheckDistance { get; private set; } = 0.75f;
 
-    [Space]
-    [Header("Wall Run Settings")]
-    [Tooltip("The height of the jump when the player jumps off a wall.")]
-    [SerializeField]
-    private float wallJumpHeight = 3f; // m
+    [field: Space]
+    [field: Header("Wall Run Settings")]
+    [field: SerializeField]
+    public float WallJumpHeight { get; private set; } = 4f;
 
-    [Tooltip("The amount of forward impulse applied to the player character when starting a wall run.")]
-    [SerializeField]
-    private float wallRunInitialImpulse = 4f; // m/s
+    [field: SerializeField]
+    public float WallRunInitialImpulse { get; private set; } = 7f;
 
-    [Space]
-    [Header("Wall Jump Settings")]
-    [Tooltip("The amount of force applied to the player character when jumping off a wall to the side.")]
-    [SerializeField]
-    private float wallJumpSideForce = 7f; // m/s
+    [field: Space]
+    [field: Header("Wall Jump Settings")]
+    [field: SerializeField]
+    public float WallJumpSideForce { get; private set; } = 6f;
 
-    [Tooltip("The amount of forward force applied to the player character when jumping off a wall.")]
-    [SerializeField]
-    private float wallJumpForwardForce = 4f; // m/s
+    [field: SerializeField]
+    public float WallJumpForwardForce { get; private set; } = 5f;
 
-    private RaycastHit _leftHitInfo;
-
-    private CharacterMovementController _characterMovementController;
-    private RaycastHit _rightHitInfo;
-
-
-    /// <summary>
-    ///     Whether the player is currently running on a wall to the right.
-    /// </summary>
     public bool IsWallRight { get; private set; }
-
-    /// <summary>
-    ///     Whether the player is currently running on a wall to the left.
-    /// </summary>
     public bool IsWallLeft { get; private set; }
-
-    /// <summary>
-    ///     Whether the player is currently running on a wall.
-    /// </summary>
     public bool IsWallRunning => IsWallRight || IsWallLeft;
+
+    private RaycastHit _rightHitInfo;
+    private RaycastHit _leftHitInfo;
+    private CharacterMovementController _characterMovementController;
 
     private void Awake()
     {
@@ -58,8 +37,6 @@ public class CharacterWallRunController : MonoBehaviour
 
     private void Update()
     {
-        // Disable movement if the player character is wall running.
-        // This is to prevent the player character from moving away from the wall.
         _characterMovementController.MovementEnabled = !IsWallRunning;
     }
 
@@ -72,26 +49,22 @@ public class CharacterWallRunController : MonoBehaviour
             return;
         }
 
-        var playerTransform = transform;
-        var playerTransformPosition = playerTransform.position;
-        var playerTransformRight = playerTransform.right;
-
-        Ray rightRay = new(playerTransformPosition, playerTransformRight);
-        Ray leftRay = new(playerTransformPosition, -playerTransformRight);
+        var rightRay = new Ray(transform.position, transform.right);
+        var leftRay = new Ray(transform.position, -transform.right);
 
         var wasWallRight = IsWallRight;
         var wasWallLeft = IsWallLeft;
 
-        IsWallRight = Physics.Raycast(rightRay, out _rightHitInfo, wallCheckDistance);
-        IsWallLeft = Physics.Raycast(leftRay, out _leftHitInfo, wallCheckDistance);
+        IsWallRight = Physics.Raycast(rightRay, out _rightHitInfo, WallCheckDistance);
+        IsWallLeft = Physics.Raycast(leftRay, out _leftHitInfo, WallCheckDistance);
 
         var boostForce = Vector3.zero;
 
         if (IsWallRight && !wasWallRight)
-            boostForce = -Vector3.Cross(_rightHitInfo.normal, transform.up) * wallRunInitialImpulse;
+            boostForce = -Vector3.Cross(_rightHitInfo.normal, transform.up) * WallRunInitialImpulse;
 
         if (IsWallLeft && !wasWallLeft)
-            boostForce = Vector3.Cross(_leftHitInfo.normal, transform.up) * wallRunInitialImpulse;
+            boostForce = Vector3.Cross(_leftHitInfo.normal, transform.up) * WallRunInitialImpulse;
 
         _characterMovementController.Rigidbody.AddForce(boostForce, ForceMode.VelocityChange);
 
@@ -99,47 +72,17 @@ public class CharacterWallRunController : MonoBehaviour
         if (IsWallLeft) _characterMovementController.Rigidbody.AddForce(-_leftHitInfo.normal, ForceMode.Acceleration);
     }
 
-    private void OnDrawGizmos()
-    {
-        var playerTransform = transform;
-        var playerTransformPosition = playerTransform.position;
-        var playerTransformRight = playerTransform.right;
-
-        Ray rightRay = new(playerTransformPosition, playerTransformRight);
-        Ray leftRay = new(playerTransformPosition, -playerTransformRight);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(rightRay.origin, rightRay.direction * wallCheckDistance);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(leftRay.origin, leftRay.direction * wallCheckDistance);
-
-        if (IsWallRight)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawSphere(_rightHitInfo.point, 0.1f);
-            Gizmos.DrawRay(_rightHitInfo.point, _rightHitInfo.normal);
-        }
-
-        if (IsWallLeft)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawSphere(_leftHitInfo.point, 0.1f);
-            Gizmos.DrawRay(_leftHitInfo.point, _leftHitInfo.normal);
-        }
-    }
-
     public void WallJump()
     {
         if (!IsWallRunning) return;
 
         var sideForce = Vector3.zero;
-        if (IsWallRight) sideForce = _rightHitInfo.normal * wallJumpSideForce;
-        if (IsWallLeft) sideForce = _leftHitInfo.normal * wallJumpSideForce;
+        if (IsWallRight) sideForce = _rightHitInfo.normal * WallJumpSideForce;
+        if (IsWallLeft) sideForce = _leftHitInfo.normal * WallJumpSideForce;
 
-        var jumpForce = Vector3.up * Mathf.Sqrt(wallJumpHeight * -2 * Physics.gravity.y);
+        var jumpForce = Vector3.up * Mathf.Sqrt(WallJumpHeight * -2 * Physics.gravity.y);
 
-        var forwardForce = transform.forward * wallJumpForwardForce;
+        var forwardForce = transform.forward * WallJumpForwardForce;
 
         var finalForce = jumpForce + sideForce + forwardForce;
 
