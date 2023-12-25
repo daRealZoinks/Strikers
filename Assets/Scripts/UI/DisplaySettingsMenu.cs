7a6f7a6fe13911fd7eeee06bb4122223f22d0021
Zoinks <murarashuvlad@gmail.com>
2023-12-25 15:23:59 +0200

    Update display settings menu and dropdowns

--- Assets/Scripts/UI/DisplaySettingsMenu.cs
+++ Assets/Scripts/UI/DisplaySettingsMenu.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class DisplaySettingsMenu : MonoBehaviour
@@ -7,24 +9,86 @@ public class DisplaySettingsMenu : MonoBehaviour
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
         var resolutionDropdown = root.Q<DropdownField>("resolutionDropdown");
+        var refreshRateDropdown = root.Q<DropdownField>("refreshRateDropdown");
         var fullscreenDropdown = root.Q<DropdownField>("fullscreenDropdown");
         var backButton = root.Q<VisualElement>("DisplayMenu").Q<Button>("backButton");
 
+        InitializeResolutionAndRefreshRateDropdown(resolutionDropdown, refreshRateDropdown);
+        InitializeFullscreenDropdown(fullscreenDropdown);
+
         resolutionDropdown.RegisterValueChangedCallback(OnResolutionChanged);
+        refreshRateDropdown.RegisterValueChangedCallback(OnRefreshRateChanged);
         fullscreenDropdown.RegisterValueChangedCallback(OnFullscreenChanged);
         backButton.clicked += OnBackClicked;
     }
 
-    private void OnResolutionChanged(ChangeEvent<string> evt)
+    private static void InitializeResolutionAndRefreshRateDropdown(DropdownField resolutionDropdown,
+        DropdownField refreshRateDropdown)
     {
-        // Implement your logic to change the resolution
-        Debug.Log("Resolution changed to " + evt.newValue);
+        var resolutions = Screen.resolutions;
+        Array.Reverse(resolutions);
+        var resolutionOptions = new List<string>();
+        var refreshRateOptions = new List<string>();
+
+        foreach (var resolution in resolutions)
+        {
+            // Add resolution to the list
+            resolutionOptions.Add(resolution.width + "x" + resolution.height);
+
+            // Add refresh rate to the list
+            refreshRateOptions.Add($"{resolution.refreshRateRatio.value:F3} Hz");
+        }
+
+        resolutionDropdown.choices = resolutionOptions;
+        refreshRateDropdown.choices = refreshRateOptions;
     }
 
-    private void OnFullscreenChanged(ChangeEvent<string> evt)
+    private static void InitializeFullscreenDropdown(DropdownField fullscreenDropdown)
     {
-        // Implement your logic to change the fullscreen setting
-        Debug.Log("Fullscreen changed to " + evt.newValue);
+        var fullscreenOptions = new List<string>
+        {
+#if UNITY_STANDALONE_WIN
+            "Exclusive Fullscreen",
+#endif
+            "Fullscreen Window",
+#if UNITY_STANDALONE_OSX
+            "Maximized Window",
+#endif
+            "Windowed"
+        };
+
+        fullscreenDropdown.choices = fullscreenOptions;
+    }
+
+    private static void OnResolutionChanged(ChangeEvent<string> evt)
+    {
+        var resolution = evt.newValue.Split('x');
+        var width = int.Parse(resolution[0]);
+        var height = int.Parse(resolution[1]);
+        Screen.SetResolution(width, height, Screen.fullScreenMode, Screen.currentResolution.refreshRateRatio);
+    }
+
+    private static void OnRefreshRateChanged(ChangeEvent<string> evt)
+    {
+        var refreshRate = uint.Parse(evt.newValue.Replace(" Hz", ""));
+        var refreshRateRatio = Screen.currentResolution.refreshRateRatio;
+        refreshRateRatio.denominator = 1;
+        refreshRateRatio.numerator = refreshRate;
+
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, Screen.fullScreenMode,
+            refreshRateRatio);
+    }
+
+    private static void OnFullscreenChanged(ChangeEvent<string> evt)
+    {
+        Screen.fullScreenMode = evt.newValue switch
+        {
+            "Exclusive Fullscreen" => FullScreenMode.ExclusiveFullScreen,
+            "Fullscreen Window" => FullScreenMode.FullScreenWindow,
+            "Maximized Window" => FullScreenMode.MaximizedWindow,
+            "Windowed" => FullScreenMode.Windowed,
+            _ => Screen.fullScreenMode
+        };
     }
 
     private void OnBackClicked()
