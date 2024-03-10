333aa466231df5236e0c06ddea49a5cede021ce8
Zoinks <murarashuvlad@gmail.com>
2024-03-11 01:56:12 +0200

    Update weapon offsets and sways

--- Assets/Scripts/WeaponOffset.cs
+++ Assets/Scripts/WeaponOffset.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponOffset : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CharacterMovementController characterMovementController;
+
+    [field: SerializeField] public float Amount { get; set; }
+    [field: SerializeField] public float MaxAmount { get; set; }
+    [field: SerializeField] public float SmoothAmount { get; set; }
+
+    private Vector3 _initialPosition;
+
+    private void Start()
     {
-        
+        _initialPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var playerVelocity = characterMovementController.Rigidbody.velocity;
+        var playerDirection = characterMovementController.transform.forward;
+
+        var relativeVelocity = Vector3.Dot(playerVelocity, playerDirection);
+
+        var velocityNormalized = -relativeVelocity * Amount * playerDirection;
+
+        velocityNormalized.x = Mathf.Clamp(velocityNormalized.x, -MaxAmount, MaxAmount);
+        velocityNormalized.y = Mathf.Clamp(velocityNormalized.y, -MaxAmount, MaxAmount);
+        velocityNormalized.z = Mathf.Clamp(velocityNormalized.z, -MaxAmount, MaxAmount);
+
+        var finalPosition = velocityNormalized + _initialPosition;
+
+        var interpolationRate = Time.deltaTime * SmoothAmount;
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition, interpolationRate);
+
+        if (playerVelocity.magnitude < 0.5f)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _initialPosition, interpolationRate);
+        }
     }
 }
--- Assets/Scripts/WeaponSway.cs
+++ Assets/Scripts/WeaponSway.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSway : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [field: SerializeField] public float Amount { get; set; }
+    [field: SerializeField] public float MaxAmount { get; set; }
+    [field: SerializeField] public float SmoothAmount { get; set; }
+
+    [SerializeField] private CharacterMovementController characterMovementController;
+
+    private Quaternion _initialRotation;
+
+    private void Start()
     {
-        
+        _initialRotation = transform.localRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var lookInput = characterMovementController.LookInput;
+
+        var rotationX = Quaternion.AngleAxis(-lookInput.y * Amount, Vector3.right);
+        var rotationY = Quaternion.AngleAxis(lookInput.x * Amount, Vector3.up);
+
+        var targetRotation = rotationX * rotationY;
+
+        var interpolationRatio = SmoothAmount * Time.deltaTime;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, interpolationRatio);
+
+        if (lookInput == Vector2.zero)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, _initialRotation, interpolationRatio);
+        }
     }
 }
