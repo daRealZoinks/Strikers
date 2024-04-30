f5cac65c587a24bb1bd1eed2a4cf32542b1882c1
Zoinks <murarashuvlad@gmail.com>
2024-04-30 13:58:59 +0300

    Add new assets and scripts for ball sound effects

--- Assets/Scripts/PlayerSoundManager.cs
+++ Assets/Scripts/PlayerSoundManager.cs
@@ -3,9 +3,20 @@ using UnityEngine;
 public class PlayerSoundManager : MonoBehaviour
 {
     [SerializeField] private SoundCue footstepsSoundCue;
+    [SerializeField] private AudioSource wallRunSound;
 
     public void PlayFootstepSound()
     {
         footstepsSoundCue.PlayRandomSound();
+    }
+
+    public void StopWallRunSound()
+    {
+        wallRunSound.mute = true;
+    }
+
+    public void PlayWallRunSound()
+    {
+        wallRunSound.mute = false;
     }
 }
