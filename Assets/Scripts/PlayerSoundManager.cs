605711539288bd3b6617f5969911b351949bb403
Zoinks <murarashuvlad@gmail.com>
2024-04-30 19:58:24 +0300

    Add new audio assets and update player sound manager

--- Assets/Scripts/PlayerSoundManager.cs
+++ Assets/Scripts/PlayerSoundManager.cs
@@ -3,20 +3,35 @@ using UnityEngine;
 public class PlayerSoundManager : MonoBehaviour
 {
     [SerializeField] private SoundCue footstepsSoundCue;
-    [SerializeField] private AudioSource wallRunSound;
+
+    [SerializeField] private AudioSource wallRunAudioSource;
+    [SerializeField] private AudioSource inAirAudioSource;
+
+    [SerializeField] private float wallRunSoundEffectVolumeSmoothTime = 0.8f;
+    [SerializeField] private float inAirSoundEffectVolumeSmoothTime = 0.8f;
+
+    [SerializeField] private CharacterMovementController characterMovementController;
+
+    private void Update()
+    {
+        var targetWallRunSoundEffectVolume = characterMovementController.IsWallRunning ? 1 : 0;
+
+        wallRunAudioSource.volume = Mathf.Lerp(wallRunAudioSource.volume,
+            targetWallRunSoundEffectVolume,
+            wallRunSoundEffectVolumeSmoothTime * Time.deltaTime);
+
+        var playerCurrentSpeed = characterMovementController.Rigidbody.velocity.magnitude;
+        var playerMaxSpeed = characterMovementController.MaxSpeed;
+
+        var targetInAirSoundEffectVolume = playerCurrentSpeed > playerMaxSpeed ? 0 : 1;
+
+        inAirAudioSource.volume = Mathf.Lerp(inAirAudioSource.volume,
+            targetInAirSoundEffectVolume,
+            inAirSoundEffectVolumeSmoothTime * Time.deltaTime);
+    }
 
     public void PlayFootstepSound()
     {
         footstepsSoundCue.PlayRandomSound();
-    }
-
-    public void StopWallRunSound()
-    {
-        wallRunSound.mute = true;
-    }
-
-    public void PlayWallRunSound()
-    {
-        wallRunSound.mute = false;
     }
 }
