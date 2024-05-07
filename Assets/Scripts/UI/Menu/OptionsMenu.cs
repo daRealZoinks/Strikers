b80f6c6bf1a773955d2242188a7a5bb13e6fa143
Zoinks <murarashuvlad@gmail.com>
2024-05-07 22:26:38 +0300

    Refactor menu navigation code for improved maintainability and readability

--- Assets/Scripts/UI/Menu/OptionsMenu.cs
+++ Assets/Scripts/UI/Menu/OptionsMenu.cs
@@ -19,56 +19,44 @@ public class OptionsMenu : MonoBehaviour
 
     private void OnGraphicsSettingsClicked()
     {
-        // Get the root VisualElement
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        // Get the main menu and options menu
         var graphicsMenu = root.Q<VisualElement>("GraphicsMenu");
         var optionsMenu = root.Q<VisualElement>("OptionsMenu");
 
-        // Hide the options menu and show the main menu
         optionsMenu.style.display = DisplayStyle.None;
         graphicsMenu.style.display = DisplayStyle.Flex;
     }
 
     private void OnDisplaySettingsClicked()
     {
-        // Get the root VisualElement
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        // Get the main menu and options menu
         var displayMenu = root.Q<VisualElement>("DisplayMenu");
         var optionsMenu = root.Q<VisualElement>("OptionsMenu");
 
-        // Hide the options menu and show the main menu
         optionsMenu.style.display = DisplayStyle.None;
         displayMenu.style.display = DisplayStyle.Flex;
     }
 
     private void OnAudioSettingsClicked()
     {
-        // Get the root VisualElement
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        // Get the main menu and options menu
         var audioMenu = root.Q<VisualElement>("AudioMenu");
         var optionsMenu = root.Q<VisualElement>("OptionsMenu");
 
-        // Hide the options menu and show the main menu
         optionsMenu.style.display = DisplayStyle.None;
         audioMenu.style.display = DisplayStyle.Flex;
     }
 
     private void OnBackClicked()
     {
-        // Get the root VisualElement
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        // Get the main menu and options menu
         var mainMenu = root.Q<VisualElement>("MainMenu");
         var optionsMenu = root.Q<VisualElement>("OptionsMenu");
 
-        // Hide the options menu and show the main menu
         optionsMenu.style.display = DisplayStyle.None;
         mainMenu.style.display = DisplayStyle.Flex;
     }
