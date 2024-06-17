dc2e540bafdda6281f66436b89e3b5a6004ef488
Zoinks <murarashuvlad@gmail.com>
2024-06-17 22:59:50 +0300

    chore: Update Player prefab position and scale, and refactor MeleeAttackController to add attack cooldown

--- Assets/Scripts/MeleeAttackController.cs
+++ Assets/Scripts/MeleeAttackController.cs
@@ -1,38 +1,85 @@
 using System.Linq;
+using Unity.Netcode;
 using UnityEngine;
 
-public class MeleeAttackController : MonoBehaviour
+public class MeleeAttackController : NetworkBehaviour
 {
-    [SerializeField]
-    private float attackForce = 1000f;
+    [SerializeField] private float attackForce = 1000f;
+    [SerializeField] private float attackCooldown = 1f;
 
-    [SerializeField]
-    private Collider playerCollider;
-
-    [SerializeField]
-    private Transform cameraTransform;
+    [SerializeField] private Collider playerCollider;
+    [SerializeField] private Transform cameraTransform;
 
     private SphereCollider _sphereCollider;
+    private float _nextAttackTime;
 
-    private readonly Collider[] hitColliders = new Collider[10];
+    private bool CanAttack => _nextAttackTime >= attackCooldown;
 
     private void Awake()
     {
         _sphereCollider = GetComponent<SphereCollider>();
     }
 
-    public void Attack()
+    private void Update()
     {
-        Physics.OverlapSphereNonAlloc(transform.position, _sphereCollider.radius, hitColliders);
+        if (IsOwner && !CanAttack)
+        {
+            _nextAttackTime += Time.deltaTime;
+        }
+    }
 
-        var filteredColliders = hitColliders.Where(c => c && c != playerCollider);
+    public void ExecuteAttack()
+    {
+        if (IsOwner && CanAttack)
+        {
+            AttackServerRpc(transform.position);
+            Attack(transform.position);
+            _nextAttackTime = 0;
+        }
+    }
+
+    [ServerRpc]
+    private void AttackServerRpc(Vector3 transformPosition)
+    {
+        AttackClientRpc(transformPosition);
+    }
+
+    [ClientRpc]
+    private void AttackClientRpc(Vector3 transformPosition)
+    {
+        if (!IsOwner)
+        {
+            Attack(transformPosition);
+        }
+    }
+
+    private void Attack(Vector3 position)
+    {
+        var colliders = Physics.OverlapSphere(position, _sphereCollider.radius);
+
+        var filteredColliders = colliders.Where(c => c && c != playerCollider);
 
         foreach (var filteredCollider in filteredColliders)
         {
             if (!filteredCollider.TryGetComponent<Rigidbody>(out var rb)) continue;
 
-            var direction = (rb.transform.position - cameraTransform.position).normalized;
-            rb.AddForce(direction * attackForce, ForceMode.Impulse);
+            var direction = cameraTransform.forward.normalized * attackForce;
+
+            // compensate for the current velocity relative to the camera
+            // if the object is moving towards the camera, add force in the opposite direction
+            // if the object is moving away from the camera, don't compensate
+
+            var velocity = rb.velocity;
+            var dot = Vector3.Dot(velocity.normalized, cameraTransform.forward);
+
+            if (dot < 0)
+            {
+                direction -= velocity;
+            }
+
+            rb.velocity = Vector3.zero;
+
+            rb.AddForce(direction, ForceMode.Impulse);
         }
     }
 }
